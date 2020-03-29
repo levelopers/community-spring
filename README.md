ee93cdd138aa89a1952ed5add235a759fedfb477
levelopers <youcancallmeallen@gmail.com>
2020-03-29 00:36:44 -0400

    integrating swagger2 api doc

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ### Spring Boot Learning Exercise 
 
 flyway migrate: ```mvn flyway:migrate```
+
+
 mybatis: ```mvn -Dmybatis.generator.overwrite=true mybatis-generator:generate```
+
+
+##### swagger path: ```/swagger-ui.html```
---
+++ src/main/java/com/forum/forum/swagger2/SpringFoxConfig.java
@@ -0,0 +1,26 @@
+package com.forum.forum.swagger2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author ：Zack
+ * @date ：Created in 2020/3/29 0:27
+ */
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
