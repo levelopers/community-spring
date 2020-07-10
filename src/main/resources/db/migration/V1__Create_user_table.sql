858296279f466982cdb05267eb5f79b9d1deac58
levelopers <youcancallmeallen@gmail.com>
2020-07-09 20:46:47 -0400

    remove quotes in t_user table

--- src/main/resources/db/migration/V1__Create_user_table.sql
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -1,11 +1,11 @@
 
 CREATE TABLE t_user
 (
- "USER_ID"      bigserial NOT NULL,
- "USERNAME"     varchar(50) NOT NULL,
- "GMT_CREATE"   bigint NOT NULL,
- "GMT_MODIFIED" bigint NOT NULL,
- "AVATAR_URL"   varchar NOT NULL,
- "PASSWORD"     varchar(100) NOT NULL,
- CONSTRAINT "PK_t_user" PRIMARY KEY ( "USER_ID" )
+ USER_ID      bigserial NOT NULL,
+ USERNAME     varchar(50) NOT NULL,
+ GMT_CREATE   bigint NOT NULL,
+ GMT_MODIFIED bigint NOT NULL,
+ AVATAR_URL   varchar NOT NULL,
+ PASSWORD     varchar(100) NOT NULL,
+ CONSTRAINT "PK_t_user" PRIMARY KEY ( USER_ID )
 );
